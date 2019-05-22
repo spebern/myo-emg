097b987dbca964092eb7bef7e61fe06a585c60c7
bold <bold@cryptoguru.com>
2019-05-22 18:47:41 +0800

    use fire for cmd line app

--- main.py
+++ main.py
@@ -7,51 +7,75 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from myo_emg_recorder import MyoEMGRecorder
 from myo_emg_preprocessor import MyoEMGPreprocessor
 import pickle
+import fire
 
 
-num_trials_per_gesture = 3
-num_channels = 8
-
-
-class Hand(Enum):
+class HandGesture(Enum):
     OPEN = 0
     CLOSE = 1
 
 
-train = False
+def gen_labels(trials, label_types):
+    """
+    Generate an array of length trials containing a random
+    sequence of label types.
+    """
+    labels = (trials // len(label_types)) * label_types
+    while len(labels) < trials:
+        labels.append(random.choice(label_types))
+    random.shuffle(labels)
+    labels = [label.value for label in labels]
+    return labels
 
-recorder = MyoEMGRecorder()
-recorder.start_recording()
-preprocessor = MyoEMGPreprocessor()
 
-if train is True:
-    cues = num_trials_per_gesture * [Hand.OPEN, Hand.CLOSE]
-    random.shuffle(cues)
+def run(trials=10, train=False):
+    """
+    Return EMG classification for HandGestures using the myo armband.
 
-    labels = [cue.value for cue in cues]
-    print(preprocessor.num_features)
-    features = np.zeros((len(cues), num_channels * preprocessor.num_features * 2))
-    print(features.shape)
-    for i, cue in enumerate(cues):
-        print(cue.name)
-        # skip one second
-        _ = recorder.get()
-        # now classify
-        signals = recorder.get()
-        features[i, :] = preprocessor.extract_features(signals)
+    train : bool
+        Train the classifier.
+    trials : int
+        Number of trials to train or predict.
+    """
 
-    clf = LinearDiscriminantAnalysis()
-    clf.fit(features, labels)
+    recorder = MyoEMGRecorder()
+    recorder.start_recording()
+    preprocessor = MyoEMGPreprocessor()
 
-    with open("trained_classifiers/clf.pkl", "wb") as fid:
-        pickle.dump(clf, fid)
+    if train is True:
+        labels = gen_labels(trials, list(HandGesture))
+        features = np.zeros(
+            (len(labels), preprocessor.num_channels * preprocessor.num_features * 2)
+        )
+        print(labels)
+        return
+        for i, label in enumerate(labels):
+            print(label.name)
+            # skip one second
+            _ = recorder.get()
+            # now classify
+            signals = recorder.get()
+            features[i, :] = preprocessor.extract_features(signals)
 
+        clf = LinearDiscriminantAnalysis()
+        clf.fit(features, labels)
+
+        with open("trained_classifiers/clf.pkl", "wb") as fid:
+            pickle.dump(clf, fid)
+    else:
+        with open("trained_classifiers/clf.pkl", "rb") as fid:
+            clf = pickle.load(fid)
+        for _ in range(trials):
+            signals = recorder.get()
+            features = preprocessor.extract_features(signals)
+            label = clf.predict([features])[0]
+            print(HandGesture(label))
     recorder.stop()
-else:
-    with open("trained_classifiers/clf.pkl", "rb") as fid:
-        clf = pickle.load(fid)
-    while True:
-        signals = recorder.get()
-        features = preprocessor.extract_features(signals)
-        label = clf.predict([features])[0]
-        print(Hand(label))
+
+
+def main():
+    fire.Fire(run)
+
+
+if __name__ == "__main__":
+    main()
